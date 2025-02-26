7fe9c0d85b12c2807b065dc9b49d2e08b1869dd6
sultanBytes <khansultand11@gmailcom>
2025-02-26 18:51:36 +0530

    pagination

--- javascript+dom/app.js
+++ javascript+dom/app.js
@@ -1,3 +1,28 @@
+let a, b;
+
+const delay = () => {
+    return new Promise((resolve, rejected) => {
+        setTimeout(() => {
+            a = 10;
+            b = 20;
+            resolve('hello')
+        }, 1000);
+
+        
+    });
+};
+
+delay().then((message)=>{
+    console.log(a + b);
+})
+.catch((error)=>{
+    console.log(error)
+})
+
+
+// delay();
+
+
 // let a;
 // let b;
 
@@ -8,19 +33,19 @@
 
 // console.log(a +b);
 
-const api = 'https://dummyjson.com/products';
+// const api = 'https://dummyjson.com/products';
 
-const readData = async () => {
-    try {
-        const response = await fetch(api);
-        console.log(response);
-    }
-    catch(error){
-        console.log(error);
-    }
-};
+// const readData = async () => {
+//     try {
+//         const response = await fetch(api);
+//         console.log(response);
+//     }
+//     catch(error){
+//         console.log(error);
+//     }
+// };
 
-readData();
+// readData();
 
 // fetch(api)
 // .then((response)=>{
