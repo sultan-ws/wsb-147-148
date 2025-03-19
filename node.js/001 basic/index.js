812c13c1981a073c8c6275401980baad3c75641a
sultanBytes <khansultand11@gmailcom>
2025-03-19 18:44:06 +0530

    npm

---
+++ node.js/001 basic/index.js
@@ -0,0 +1,47 @@
+const { a, b, c } = require('./support');
+
+console.log(a, b, c);
+
+
+
+// const arr1 = [10, 20, 30, 40, 50, 60];
+// const arr2 = [1, 2, 3, 4, 5,6];
+
+// arr1.splice(2, 2, arr2);
+// const arr3 = arr1.flat();
+
+// let result = [];
+
+// for(let i = 0; i < arr3.length; i++) {
+//     if(arr3[i] !== arr1[10]){
+//         result.push(arr3[i])
+//     }
+// };
+
+// console.log(result);
+
+/** */
+// arr1[2] = arr2;
+
+// const arr3 = [...arr1.flat()];
+// let result = 0;
+
+// for(let v of arr3){
+//     result -= v;
+// }
+
+// console.log(result);
+
+/*** */
+
+// arr1[2] = arr2;
+// let sum = 0;
+
+// for(let v of arr1) {
+//     if(typeof(v) == 'number'){
+
+//         sum++;
+//     }
+// };
+
+// console.log(sum);
